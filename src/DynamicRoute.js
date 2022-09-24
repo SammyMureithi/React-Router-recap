ec9db8b1539305b8ad345bbf15ed3dd1af51b3b8
Sammy Mureithi <sammymureithi20@gmail.com>
2022-09-24 12:42:17 +0300

    UseParams

--- src/DynamicRoute.js
+++ src/DynamicRoute.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function DynamicRoute() {
+    const { userId } = useParams();
   return (
       <div>
-          <h1>Am a dynamic Route</h1>
+          <h1>Am a dynamic Route  with {userId} param</h1>
     </div>
   )
 }
